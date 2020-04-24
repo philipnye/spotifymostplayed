12bae1075f052a46889c441aeac962752d459231
Philip Nye <contact@philipnye.co.uk>
2020-04-24 09:17:16 +0100

    Make more data available to the spotify_search function

--- extract_data.py
+++ extract_data.py
@@ -6,7 +6,13 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())		# SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT_SECRET environment variables have been set in Windows
 
-def spotify_search(artists,track):
+def spotify_search(row):
+	track=row['track']
+	artists=re.split(r'\sand\s|\s&\s|,\s|\sfeaturing\s', row['artist'])
+	collaboration=False
+	if re.findall(r'\sand\s|\s&\s|,\s|\sfeaturing\s', row['artist']):		# this works fine for the current top 100, but we'll come unstuck when new artist '666 featuring & 99' hits the streets. 'and' is case sensitive and ignores 'And's in artist names.
+		collaboration=True
+
 	q_track = track.replace('\'','')		# the API is tripped up by apostrophes
 	q_artists=' '.join(artists).replace('\'','')		# turns artists list into a string of artist names separated by spaces, which can be used in the search string
 	q_str = 'track:"' + q_track + '" artist:' + q_artists		# spotipy accepts artist names separated by a space
@@ -38,9 +44,4 @@ def spotify_search(artists,track):
 with open('list.csv', newline='') as infile:
 	reader = csv.DictReader(infile)
 	for row in reader:
-		collaboration=False
-		track=row['track']
-		artists=re.split(r'\sand\s|\s&\s|,\s|\sfeaturing\s', row['artist'])
-		if re.findall(r'\sand\s|\s&\s|,\s|\sfeaturing\s', row['artist']):		# this works fine for the current top 100, but we'll come unstuck when new artist '666 featuring & 99' hits the streets. 'and' is case sensitive and ignores 'And's in artist names.
-			collaboration=True
-		spotify_search(artists,track)
+		spotify_search(row)
