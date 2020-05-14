5ed1eab84ba27aaaba0f4d6b60c5bf1c7930e42a
Philip Nye <contact@philipnye.co.uk>
2020-05-14 09:14:48 +0100

    Improve readability of spotify_track_search()

--- extract_data.py
+++ extract_data.py
@@ -16,14 +16,13 @@ from spotipy.oauth2 import SpotifyClientCredentials
 sp = spotipy.Spotify(client_credentials_manager = SpotifyClientCredentials())		# SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT_SECRET environment variables have been set in Windows
 
 def spotify_track_search(row):
-	title = row['title']
-	artists = re.split(r'\sand\s|\s&\s|,\s|\sfeaturing\s', row['artist'])
+	artists = re.split(r'\sand\s|\s&\s|,\s|\sfeaturing\s', df.at[index, 'artist'])
 
 	collaboration = False
-	if re.findall(r'\sand\s|\s&\s|,\s|\sfeaturing\s', row['artist']):		# this works fine for the current top 100, but we'll come unstuck when new artist '666 featuring & 99' hits the streets. 'and' is case sensitive and ignores 'And's in artist names.
+	if re.findall(r'\sand\s|\s&\s|,\s|\sfeaturing\s', df.at[index, 'artist']):		# this works fine for the current top 100, but we'll come unstuck when new artist '666 featuring & 99' hits the streets. 'and' is case sensitive and ignores 'And's in artist names.
 		collaboration = True
 
-	q_title = title.replace('\'', '')		# the API is tripped up by apostrophes
+	q_title = df.at[index, 'title'].replace('\'', '')		# the API is tripped up by apostrophes
 	q_artists = ' '.join(artists).replace('\'', '')		# turns artists list into a string of artist names separated by spaces, which can be used in the search string
 	q_str = 'track:"' + q_title + '" artist:' + q_artists		# spotipy accepts artist names separated by a space
 	result = sp.search(q = q_str, type = 'track', limit = 50)
@@ -35,12 +34,12 @@ def spotify_track_search(row):
 	df.at[index, 'collaboration'] = collaboration
 
 	for track_item in result['tracks']['items']:
-		track_title = re.split(r' \(feat\..*\)', track_item['name'])[0]		# ditch details of collaborators from the track name that Spotify sometimes adds e.g. '(feat. Young Thug)'
-		track_title = re.split(r' \(with .*\)', track_title)[0]		# handle a couple more cases of collaborators being listed in the track name. Fine until there's a track title that legitimately contains this string pattern...
-		if track_title.lower() == title.lower():		# in the absence of an exact match functionality in the API search
+		spotify_title = re.split(r' \(feat\..*\)', track_item['name'])[0]		# ditch details of collaborators from the track name that Spotify sometimes adds e.g. '(feat. Young Thug)'
+		spotify_title = re.split(r' \(with .*\)', spotify_title)[0]		# handle a couple more cases of collaborators being listed in the track name. Fine until there's a track title that legitimately contains this string pattern...
+		if spotify_title.lower() == df.at[index, 'title'].lower():		# in the absence of an exact match functionality in the API search
 			if track_item['album']['release_date_precision'] == 'day':		# some albums only have precision in years or months, not days
 				if datetime.strptime(track_item['album']['release_date'], '%Y-%m-%d') < df.at[index, 'release_date']:
-					df.at[index, 'title'] = track_title		# go with the styling conventions applied in Spotify
+					df.at[index, 'title'] = spotify_title		# go with the styling conventions applied in Spotify
 					df.at[index, 'artists'].clear()
 					df.at[index, 'spotify_artist_IDs'].clear()
 					for artist in track_item['artists']:
@@ -51,7 +50,7 @@ def spotify_track_search(row):
 					df.at[index, 'duration_ms'] = track_item['duration_ms']
 			elif track_item['album']['release_date_precision'] == 'year':
 				if int(track_item['album']['release_date']) < df.at[index, 'release_date'].year:
-					df.at[index, 'title'] = track_title
+					df.at[index, 'title'] = spotify_title
 					df.at[index, 'artists'].clear()
 					df.at[index, 'spotify_artist_IDs'].clear()
 					for artist in track_item['artists']:
@@ -61,7 +60,7 @@ def spotify_track_search(row):
 					df.at[index, 'release_date'] = datetime.strptime(track_item['album']['release_date'], '%Y')
 					df.at[index, 'duration_ms'] = track_item['duration_ms']
 			else:
-				print(track_title + ' has release date precision of ' + track_item['album']['release_date_precision'])
+				print(spotify_title + ' has release date precision of ' + track_item['album']['release_date_precision'])
 
 def spotify_artist_search(row, artist):
 	q_str = 'artist:' + artist
@@ -73,8 +72,14 @@ def spotify_artist_search(row, artist):
 			df.at[index, 'followers'].append(artist_item['followers']['total'])
 			break		# takes the top artist, matching exactly on artist name
 
+def genius_track_search(row):
+	# q_title =
+	# q_artists =
+	q_str = 'track:"' + q_title + '" artist:' + q_artists		# spotipy accepts artist names separated by a space
+	result = genius.search_song('Andy Shauf', max_songs = 3, sort = 'title')
 
-df = pd.read_csv('list.csv', index_col = 0, encoding = 'cp1252')		# standard Windows encoding
+
+df = pd.read_csv('list_short.csv', index_col = 0, encoding = 'cp1252')		# standard Windows encoding
 df['artists'] = None		# add column
 df['artists'] = df['artists'].astype('object')		# set datatype to object, so that artists column can hold a list
 df['spotify_artist_IDs'] = None
